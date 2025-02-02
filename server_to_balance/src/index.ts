b9e284907c25a91ce195eacd32708ac320ef39e3
Alfazal007 <alfazal7777@gmail.com>
2025-02-02 22:10:15 +0530

    added test post endpoint

--- server_to_balance/src/index.ts
+++ server_to_balance/src/index.ts
@@ -6,10 +6,17 @@ configDotenv();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/hello", (_req: Request, res: Response) => {
 	res.status(200).json({
 		message: "hello"
 	});
+	return;
+});
+
+app.post("/sendData", (req: Request, res: Response) => {
+	res.status(200).json(req.body);
 	return;
 });
 
